2d4df4d8c883482a3054cacd19f247a4e6d6fe0d
Thomas Glaser <thomas@tkglaser.net>
2019-04-20 17:04:43 +0100

    Remove reference

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,6 +1,5 @@
 export * from './clock.service';
 export * from './config.service';
-export * from './entry.service';
 export * from './logging.service';
 export * from './unique-id.service';
 export * from './update.service';

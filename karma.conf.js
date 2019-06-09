8120a8382dcfc40c3e2390ff333512e7153bc4cb
Thomas Glaser <thomas@tkglaser.net>
2019-06-09 16:22:11 +0100

    PhantomJS for tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-process.env.CHROME_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -9,7 +8,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -27,8 +26,12 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['PhantomJS'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    phantomjsLauncher: {
+        // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+        exitOnResourceError: true
+      }
   });
 };

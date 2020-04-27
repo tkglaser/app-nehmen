c90e31971586c4a8e1a51c3a9721c5aaf22fc501
Thomas Glaser <thomas@tkglaser.net>
2020-04-27 11:28:09 +0100

    susi

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,7 +1,7 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const DiscoveryDocumentConfig = {
-  url : "https://appnehmen.b2clogin.com/appnehmen.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=b2c_1_appnehmen"
+  url : "https://appnehmen.b2clogin.com/appnehmen.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=b2c_1_susi"
 }
 
 export const authConfig: AuthConfig = {
@@ -9,8 +9,8 @@ export const authConfig: AuthConfig = {
   responseType: 'token id_token',
   issuer: 'https://appnehmen.b2clogin.com/f17a7148-c2f7-4d1a-9390-a351e1328255/v2.0/',
   strictDiscoveryDocumentValidation: false,
-  tokenEndpoint: 'https://appnehmen.b2clogin.com/appnehmen.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_appnehmen',
-  loginUrl: 'https://appnehmen.b2clogin.com/appnehmen.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_appnehmen',
+  tokenEndpoint: 'https://appnehmen.b2clogin.com/appnehmen.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_susi',
+  loginUrl: 'https://appnehmen.b2clogin.com/appnehmen.onmicrosoft.com/oauth2/v2.0/token?p=b2c_1_susi',
   clientId: '2547fe20-84af-40ac-8405-2e7a6b93d972',
   scope: 'openid profile https://appnehmen.onmicrosoft.com/appnehmen-api/user_impersonation',
   skipIssuerCheck: true,
